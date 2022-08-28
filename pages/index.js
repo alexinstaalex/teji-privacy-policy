8cdf38da0ecc4d280b8401d309976974f8dd7d5b
alexinstaalex <100771510+alexinstaalex@users.noreply.github.com>
2022-08-28 17:15:20 +0530

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,19 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>TEJI - Privacy Policy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="TEJI - Privacy Policy" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We do not collect any of your data.
+          The data provided is processed to provide the results to the users.
         </p>
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
